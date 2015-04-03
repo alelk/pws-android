3fcca00d626794202d2f4cbf4d274ba90b242bd0
Alex Elkin <elckinne@gmail.com>
2015-04-03 11:12:40 +0300

    Chapter and Book classes have been added. Psalm class has been updated. The functionality of PwsXmlParser is changed.

---
+++ database/src/main/java/com/alelk/pws/database/data/Book.java
@@ -0,0 +1,178 @@
+package com.alelk.pws.database.data;
+
+import java.util.Date;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Created by alelkin on 31.03.2015.
+ */
+public class Book implements PwsObject {
+    private String name;
+    private String shortName;
+    private String displayName;
+    private String description;
+    private String version;
+    private BookEdition edition;
+    private Date releaseDate;
+    private List<String> authors;
+    private List<String> creators;
+    private List<String> reviewers;
+    private List<String> editors;
+    private String comment;
+
+    private SortedMap<Integer, Chapter> chapters = new TreeMap<>();
+    private SortedMap<Integer, Psalm> psalms = new TreeMap<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public BookEdition getEdition() {
+        return edition;
+    }
+
+    public void setEdition(BookEdition edition) {
+        this.edition = edition;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public List<String> getCreators() {
+        return creators;
+    }
+
+    public void setCreators(List<String> creators) {
+        this.creators = creators;
+    }
+
+    public List<String> getReviewers() {
+        return reviewers;
+    }
+
+    public void setReviewers(List<String> reviewers) {
+        this.reviewers = reviewers;
+    }
+
+    public List<String> getEditors() {
+        return editors;
+    }
+
+    public void setEditors(List<String> editors) {
+        this.editors = editors;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Psalm getPsalm(int number) {
+        return psalms.get(number);
+    }
+
+    public SortedMap<Integer, Psalm> getPsalms() {
+        return psalms;
+    }
+
+    public void setPsalms(SortedMap<Integer, Psalm> psalms) {
+        this.psalms = new TreeMap<>();
+        if (psalms != null && psalms.size() > 0) {
+            this.psalms.putAll(psalms);
+        }
+    }
+
+    public SortedMap<Integer, Chapter> getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(SortedMap<Integer, Chapter> chapters) {
+        this.chapters = new TreeMap<>();
+        if (chapters != null && chapters.size() > 0){
+            this.chapters.putAll(chapters);
+        }
+    }
+
+    public String toString() {
+        String s = "Book v" + this.version + " {" +
+                "name='" + this.name +
+                "' shortName='" + this.shortName +
+                "' displayName='" + this.displayName +
+                "' description='" + this.description +
+                "' edition='" + this.edition +
+                "' releaseDate='" + this.releaseDate +
+                "' comment='" + this.comment +
+                "' authors=[ ";
+        for (String author : this.authors) {
+            s += "'" + author + "' ";
+        }
+        s += "] creators=[ ";
+        for (String creator : this.creators) {
+            s += "'" + creator + "' ";
+        }
+        s += "] editors=[ ";
+        for (String editor : this.editors) {
+            s += "'" + editor + "' ";
+        }
+        s += "] reviewers=[ ";
+        for (String reviewer : this.reviewers) {
+            s += "'" + reviewer + "' ";
+        }
+        s += "]}";
+        return s;
+    }
+}
--- xmlengine/src/main/java/com/alelk/pws/xmlengine/Constants.java
+++ xmlengine/src/main/java/com/alelk/pws/xmlengine/Constants.java
@@ -3,13 +3,14 @@ package com.alelk.pws.xmlengine;
 /**
  * Created by alelkin on 25.03.2015.
  */
-public abstract class Constants {
+public interface Constants {
 
     /**
      * Supported PWS XML tags
      */
     public final static class TAG {
         public final static class PSLM {
+            public static final String EDITION = BK.EDITION;
             public static final String TAG = "psalm";
             public static final String AUTHOR = "author";
             public static final String TRANSLATOR = "translator";
@@ -19,6 +20,7 @@ public abstract class Constants {
             public static final String COMPOSER = "composer";
             public static final String YEAR = "year";
             public static final String TEXT = "text";
+            public static final String TONALITIES = "tonalities";
             public final static class NUMS {
                 public static final String TAG = Constants.TAG.PSLM.NUMBERS;
                 public static final String NUMBER = "number";
@@ -34,10 +36,47 @@ public abstract class Constants {
                     public static final String NUMBER = "number";
                 }
             }
+            public final static class TONS {
+                public static final String TAG = PSLM.TONALITIES;
+                public static final String TONALITY = "tonality";
+            }
         }
         public final static class BK {
             public static final String TAG = "book";
+            public static final String VERSION = "version";
+            public static final String NAME = "name";
+            public static final String SHORT_NAME = "shortname";
+            public static final String DISPLAY_NAME = "displayname";
             public static final String EDITION = "edition";
+            public static final String DESCRIPTION = "description";
+            public static final String RELEASE_DATE = "releasedate";
+            public static final String COMMENT = "comment";
+            public static final String AUTHORS = "authors";
+            public static final String CREATORS = "creators";
+            public static final String REVIEWERS = "reviewers";
+            public static final String EDITORS = "editors";
+            public static final String PSALMS = "psalms";
+            public final static class CRTRS {
+                public static final String TAG = BK.CREATORS;
+                public static final String CREATOR = "creator";
+            }
+            public final static class RVRS {
+                public static final String TAG = BK.REVIEWERS;
+                public static final String REVIEWER = "reviewer";
+            }
+            public final static class EDTRS {
+                public static final String TAG = BK.EDITORS;
+                public static final String EDITOR = "editor";
+            }
+            public final static class AUTRS {
+                public static final String TAG = BK.AUTHORS;
+                public static final String AUTHOR = "author";
+            }
+            public final static class PSLMS {
+                public static final String TAG = BK.PSALMS;
+                public static final String PSALM = PSLM.TAG;
+                public static final String REF = "ref";
+            }
         }
     }
 
