48b0333c8d56affd730ba7f2116958ec4b081c44
Alex Elkin <elckinne@gmail.com>
2015-04-17 23:57:14 +0300

    Activities design has been started: MainActivity, PsalmActivity. PsalmListAdapter has been added.

---
+++ app/src/main/java/com/alelk/pws/pwapp/PsalmListAdapter.java
@@ -0,0 +1,46 @@
+package com.alelk.pws.pwapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.alelk.pws.database.data.BookEdition;
+import com.alelk.pws.database.data.Psalm;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 17.04.2015.
+ */
+public class PsalmListAdapter extends ArrayAdapter<Psalm> {
+
+    List<Psalm> objects;
+    public PsalmListAdapter(Context context, int resource, List<Psalm> objects) {
+        super(context, resource, objects);
+        this.objects = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.psalms_list_layout, null);
+        }
+
+        Psalm psalm = objects.get(position);
+        if (psalm != null) {
+            TextView psalmNumber = (TextView) v.findViewById(R.id.psalm_number);
+            TextView psalmName = (TextView) v.findViewById(R.id.psalm_name);
+            psalmNumber.setText(Integer.toString(psalm.getNumber(BookEdition.GUSLI)));
+            psalmName.setText(psalm.getName());
+        }
+        return v;
+    }
+
+
+}
