c5fba20e31dbf743493616467f36f9f845257905
Alex Elkin <alelkdev@gmail.com>
2015-04-22 19:26:35 +0300

    The new Pws Database Helpers have been added

---
+++ database/src/main/java/com/alelk/pws/database/helper/PwsDatabaseChorusHelper.java
@@ -0,0 +1,44 @@
+package com.alelk.pws.database.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by alelkin on 22.04.2015.
+ */
+public class PwsDatabaseChorusHelper extends SQLiteOpenHelper implements PwsDatabaseHelper {
+    private static final String LOG_TAG = PwsDatabaseChorusHelper.class.getName();
+
+    public static final String TABLE_CHORUSES = "choruses";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NUMBER = "number";
+    public static final String COLUMN_PSALM_ID = "psalmid";
+    public static final String COLUMN_TEXT = "text";
+
+    private static final String TABLE_CREATE_SCRIPT = "create table " + TABLE_CHORUSES +
+            "(" + COLUMN_ID + " integer primary key autoincrement, " +
+            COLUMN_NUMBER + " text not null, " +
+            COLUMN_PSALM_ID + " integer not null " +
+            COLUMN_TEXT + " text);";
+
+    private static final String TABLE_DROP_SCRIPT = "drop table if exists " + TABLE_CHORUSES;
+
+    public PwsDatabaseChorusHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE_SCRIPT);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i(LOG_TAG, "Upgrade PWS database table '" + TABLE_CHORUSES +
+                "' from version " + oldVersion + " to version " + newVersion);
+        db.execSQL(TABLE_DROP_SCRIPT);
+        onCreate(db);
+    }
+}
