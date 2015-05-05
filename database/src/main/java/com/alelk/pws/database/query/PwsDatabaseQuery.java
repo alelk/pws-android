a5c430ff0fddad2e8cf1a8d4d32f307952876b2e
Alex Elkin <elckinne@gmail.com>
2015-05-05 19:32:52 +0300

    Pws Database Queries have been changed: PwsDatabaseBookQuery PwsDatabaseChorusQuery PwsDatabasePsalmNumberQuery PwsDatabasePsalmQuery PwsDatabaseVerseQuery. The psalm numbers insert functionality has been implemented.

--- database/src/main/java/com/alelk/pws/database/query/PwsDatabaseQuery.java
+++ database/src/main/java/com/alelk/pws/database/query/PwsDatabaseQuery.java
@@ -2,6 +2,8 @@ package com.alelk.pws.database.query;
 
 import com.alelk.pws.database.data.PwsObject;
 import com.alelk.pws.database.data.entity.PwsDatabaseEntity;
+import com.alelk.pws.database.exception.PwsDatabaseException;
+import com.alelk.pws.database.exception.PwsDatabaseIncorrectValueException;
 import com.alelk.pws.database.exception.PwsDatabaseSourceIdExistsException;
 
 /**
@@ -15,14 +17,14 @@ public interface PwsDatabaseQuery <T extends PwsObject, E extends PwsDatabaseEnt
      * Insert PwsObject into Pws database.
      * @param pwsObject The PwsObject to insert
      * @return PwsDatabaseEntity inserted into database.
-     * @throws PwsDatabaseSourceIdExistsException if PwsObject already exists in database
+     * @throws PwsDatabaseException if any problems
      */
-    E insert(T pwsObject) throws PwsDatabaseSourceIdExistsException;
+    E insert(T pwsObject) throws PwsDatabaseException;
 
     /**
      * Returns the PwsDatabaseEntity from Pws database selected by id
      * @param id the id of PwsDatabaseEntity to select
      * @return PwsDatabaseEntity with specified id, null if no PwsDatabaseEntity found
      */
-    E selectById(long id);
+    E selectById(long id) throws PwsDatabaseIncorrectValueException;
 }
