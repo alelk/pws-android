21860fe1305b7a1187e17d5a2cd738addde16eae
Alex Elkin <elckinne@gmail.com>
2015-05-07 22:57:24 +0300

    The queries have been updated

--- database/src/main/java/com/alelk/pws/database/source/PwsDataSource.java
+++ database/src/main/java/com/alelk/pws/database/source/PwsDataSource.java
@@ -1,11 +1,14 @@
 package com.alelk.pws.database.source;
 
 import com.alelk.pws.database.data.Book;
+import com.alelk.pws.database.data.BookEdition;
 import com.alelk.pws.database.data.Psalm;
 import com.alelk.pws.database.data.entity.BookEntity;
 import com.alelk.pws.database.data.entity.PsalmEntity;
 import com.alelk.pws.database.exception.PwsDatabaseIncorrectValueException;
 import com.alelk.pws.database.exception.PwsDatabaseSourceIdExistsException;
+
+import java.util.Map;
 
 /**
  * Created by alelkin on 23.04.2015.
@@ -15,4 +18,5 @@ public interface PwsDataSource {
     void close();
     BookEntity addBook(Book book);
     PsalmEntity addPsalm(Psalm psalm) throws PwsDatabaseSourceIdExistsException, PwsDatabaseIncorrectValueException;
+    Map<Integer, Psalm> getPsalms(BookEdition bookEdition) throws PwsDatabaseIncorrectValueException;
 }
