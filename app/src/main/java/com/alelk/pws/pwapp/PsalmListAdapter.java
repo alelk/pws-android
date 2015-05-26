64e9372d675a2910967ab167a933db72e1fe8005
Alex Elkin <alelkdev@gmail.com>
2015-05-26 20:55:11 +0300

    working on suggestions functionality

--- app/src/main/java/com/alelk/pws/pwapp/PsalmListAdapter.java
+++ app/src/main/java/com/alelk/pws/pwapp/PsalmListAdapter.java
@@ -13,7 +13,7 @@ import com.alelk.pws.database.data.Psalm;
 import java.util.List;
 
 /**
- * Created by Alex on 17.04.2015.
+ * Created by Alex Elkin on 17.04.2015.
  */
 public class PsalmListAdapter extends ArrayAdapter<Psalm> {
 
@@ -25,6 +25,7 @@ public class PsalmListAdapter extends ArrayAdapter<Psalm> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // todo rewrite this method
         View v = convertView;
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -35,8 +36,21 @@ public class PsalmListAdapter extends ArrayAdapter<Psalm> {
         if (psalm != null) {
             TextView psalmNumber = (TextView) v.findViewById(R.id.txt_psalm_number);
             TextView psalmName = (TextView) v.findViewById(R.id.txt_psalm_name);
+            TextView bookEdition = (TextView) v.findViewById(R.id.txt_book_edition);
+            TextView anotherBookEditions = (TextView) v.findViewById(R.id.txt_another_book_editions);
             psalmNumber.setText(Integer.toString(psalm.getNumber(BookEdition.PV3055)));
             psalmName.setText(psalm.getName());
+            bookEdition.setText(BookEdition.PV3055.getSignature());
+            String anotherEditions = "";
+            for (BookEdition be : psalm.getBookEditions()) {
+                if (!be.equals(BookEdition.PV3055)) {
+                    if (anotherEditions.length() > 0) {
+                        anotherEditions += "; ";
+                    }
+                    anotherEditions += psalm.getNumber(be) + ": " + be.getSignature();
+                }
+            }
+            anotherBookEditions.setText(anotherEditions);
         }
         return v;
     }
