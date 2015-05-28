acb83b4b1261b7efdecc3c52e522dac83e27c68b
Alex Elkin <elckinne@gmail.com>
2015-05-28 16:20:08 +0300

    some psalms have been added for book edition PV3055: numbers 71-90

--- app/src/main/java/com/alelk/pws/pwapp/MainActivity.java
+++ app/src/main/java/com/alelk/pws/pwapp/MainActivity.java
@@ -69,8 +69,11 @@ public class MainActivity extends ActionBarActivity {
                     "pwsbooks/sdp.pwsbk",
                     "pwsbooks/tympan.pwsbk",
                     "pwsbooks/slavit\'_bez_zapinki.pwsbk",
-                    "pwsbooks/fcpsalms.pwsbk", "pwsbooks/PVNL.pwsbk",
-                    "pwsbooks/fcpsalms.pwsbk", "pwsbooks/Zarja.pwsbk");
+                    "pwsbooks/fcpsalms.pwsbk",
+                    "pwsbooks/PVNL.pwsbk",
+                    "pwsbooks/fcpsalms.pwsbk",
+                    "pwsbooks/Zarja.pwsbk",
+                    "pwsbooks/Svirel.pwsbk");
 
             Map<BookEdition, Book> books = new HashMap();
 
