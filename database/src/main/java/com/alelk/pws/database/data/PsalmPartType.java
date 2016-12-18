8c6f5661ddb87423b823b65b13ba0322f88b4159
Alex Elkin <alelkdev@gmail.com>
2016-12-18 21:18:51 +0300

    Merge feature to develop. Preparing app release 1.0.1

--- database/src/main/java/com/alelk/pws/database/data/PsalmPartType.java
+++
@@ -1,9 +0,0 @@
-package com.alelk.pws.database.data;
-
-/**
- * Created by alelkin on 25.03.2015.
- */
-public enum PsalmPartType {
-    VERSE,
-    CHORUS
-}
--- database/src/main/java/com/alelk/pws/database/provider/PwsDataProviderContract.java
+++
@@ -1,8 +0,0 @@
-package com.alelk.pws.database.provider;
-
-/**
- * Created by Alex Elkin on 21.05.2015.
- */
-public final class PwsDataProviderContract {
-    public static final String AUTHORITY = "com.alelk.pws.database.provider";
-}
