17cf5dfc0e90140a49c256f0f6f329c41f3e0483
Alex Elkin <elckinne@gmail.com>
2016-12-18 21:18:51 +0300

    Merge feature to develop. Preparing app release 1.0.1

--- app/src/main/java/com/alelk/pws/pwapp/data/PwsParcelableObject.java
+++
@@ -1,9 +0,0 @@
-package com.alelk.pws.pwapp.data;
-
-import android.os.Parcelable;
-
-/**
- * Created by Alex on 18.04.2015.
- */
-interface PwsParcelableObject extends Parcelable {
-}
--- database/src/main/java/com/alelk/pws/database/table/PwsTable.java
+++
@@ -1,9 +0,0 @@
-package com.alelk.pws.database.table;
-
-import android.database.sqlite.SQLiteDatabase;
-
-/**
- * Created by alelkin on 21.04.2015.
- */
-public interface PwsTable {
-}
