1a019d55a37e5aebce8f879ce43f1a14e5cf228e
Alex Elkin <elckinne@gmail.com>
2022-10-21 02:48:30 +0300

    refactor(database): Migrate module to Kotlin.

--- pws/database/src/main/java/com/alelk/pws/database/table/PwsTable.kt
+++ pws/database/src/main/java/com/alelk/pws/database/table/PwsTable.kt
@@ -13,14 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package com.alelk.pws.database.table;
+package com.alelk.pws.database.table
 
 /**
  * Pws Table Interface
  *
  * Created by Alex Elkin on 21.04.2015.
  */
-public interface PwsTable {
-
-}
+interface PwsTable 
