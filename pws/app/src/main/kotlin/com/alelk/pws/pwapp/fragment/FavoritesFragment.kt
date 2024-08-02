334d1a51283f76b6be74c0b8152453072e331dca
Mykhailo Dmytriakha <mykhailo_dmytriakha@bcbsil.com>
2024-08-02 00:24:11 -0700

    fix empty screen issue for main activity after power off/on device

--- pws/app/src/main/kotlin/com/alelk/pws/pwapp/fragment/FavoritesFragment.kt
+++ pws/app/src/main/kotlin/com/alelk/pws/pwapp/fragment/FavoritesFragment.kt
@@ -15,6 +15,7 @@
  */
 package com.alelk.pws.pwapp.fragment
 
+import android.content.Context
 import android.content.Intent
 import android.database.Cursor
 import android.os.Bundle
@@ -41,13 +42,18 @@ import com.alelk.pws.pwapp.adapter.FavoritesRecyclerViewAdapter
  * Created by Alex Elkin on 18.02.2016.
  */
 class FavoritesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
-  private var sortOrder: Int? = null
+  private var sortOrder = SORT_BY_ADDED_DATE // by default
+  private val sharedPrefs by lazy {
+    requireActivity().getSharedPreferences("favorites_prefs", Context.MODE_PRIVATE)
+  }
   private var mFavoritesAdapter: FavoritesRecyclerViewAdapter? = null
+  private var menuSortByAddedDate: MenuItem? = null
   private var menuSortByName: MenuItem? = null
   private var menuSortByNumber: MenuItem? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    sortOrder = sharedPrefs.getInt(KEY_SORTED_BY, SORT_BY_ADDED_DATE)
     setHasOptionsMenu(true)
   }
 
@@ -73,6 +79,14 @@ class FavoritesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     return v
   }
 
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    with(sharedPrefs.edit()) {
+      putInt(KEY_SORTED_BY, sortOrder)
+      apply()
+    }
+  }
+
   override fun onCreateLoader(loaderId: Int, args: Bundle?): Loader<Cursor> =
     when (loaderId) {
       PWS_FAVORITES_LOADER -> CursorLoader(
@@ -84,6 +98,7 @@ class FavoritesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
         when (sortOrder) {
           SORT_BY_NUMBER -> "${PwsDataProviderContract.Favorites.COLUMN_PSALMNUMBER} ASC"
           SORT_BY_NAME -> "${PwsDataProviderContract.Favorites.COLUMN_PSALMNAME} ASC"
+          SORT_BY_ADDED_DATE -> PwsDataProviderContract.Favorites.SORT_ORDER
           else -> null
         }
       )
@@ -92,13 +107,14 @@ class FavoritesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     }
 
   override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-    menuSortByName = menu.add(0, 1, 0, R.string.sort_by_name)
-    menuSortByNumber = menu.add(0, 2, 0, R.string.sort_by_number)
+    menuSortByAddedDate = menu.add(0, 1, 0, R.string.sort_by_added_date)
+    menuSortByName = menu.add(0, 2, 0, R.string.sort_by_name)
+    menuSortByNumber = menu.add(0, 3, 0, R.string.sort_by_number)
     super.onCreateOptionsMenu(menu, inflater)
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    return when (item.itemId) {
+    val result = when (item.itemId) {
       menuSortByNumber!!.itemId -> {
         this.sortOrder = SORT_BY_NUMBER
         mFavoritesAdapter?.updateView()
@@ -113,8 +129,20 @@ class FavoritesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
         true
       }
 
+      menuSortByName!!.itemId -> {
+        this.sortOrder = SORT_BY_ADDED_DATE
+        mFavoritesAdapter?.updateView()
+        loaderManager.restartLoader(PWS_FAVORITES_LOADER, null, this)
+        true
+      }
+
       else -> super.onOptionsItemSelected(item)
     }
+    with(sharedPrefs.edit()) {
+      putInt(KEY_SORTED_BY, sortOrder)
+      apply()
+    }
+    return result
   }
 
   override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor) {
@@ -127,7 +155,9 @@ class FavoritesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
   companion object {
     const val PWS_FAVORITES_LOADER = 1
-    const val SORT_BY_NUMBER = 2
-    const val SORT_BY_NAME = 3
+    const val SORT_BY_ADDED_DATE = 2
+    const val SORT_BY_NUMBER = 3
+    const val SORT_BY_NAME = 4
+    private const val KEY_SORTED_BY = "favorites-sorted-by"
   }
 }
