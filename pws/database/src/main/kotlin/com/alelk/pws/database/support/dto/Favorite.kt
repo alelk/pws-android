503e4c52026329b06874aa0c32afd0dcc234d13a
Alex Elkin <alelkdev@gmail.com>
2024-09-28 20:00:04 +0300

    refactor(database): remove room files

--- pws/database/src/main/kotlin/com/alelk/pws/database/support/dto/Favorite.kt
+++
@@ -1,9 +0,0 @@
-package com.alelk.pws.database.support.dto
-
-import androidx.room.ColumnInfo
-
-data class Favorite(
-  @ColumnInfo("edition") val bookExternalId: String,
-  @ColumnInfo("number") val songNumber: Int,
-  @ColumnInfo("position") val position: Int
-)
