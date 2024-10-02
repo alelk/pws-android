71ea5c2ee01a640d7ece16c57eaecb5bfe025f74
Alex Elkin <alelkdev@gmail.com>
2024-10-02 19:34:52 +0300

    feat(app): reimplement history and favorites fragment using room dao

--- pws/app/src/main/kotlin/com/alelk/pws/pwapp/fragment/HistoryFragment.kt
+++ pws/app/src/main/kotlin/com/alelk/pws/pwapp/fragment/HistoryFragment.kt
@@ -15,118 +15,90 @@
  */
 package com.alelk.pws.pwapp.fragment
 
-import android.content.Context
+import android.app.Application
 import android.content.Intent
-import android.database.Cursor
 import android.os.Bundle
-import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.loader.app.LoaderManager
-import androidx.loader.content.CursorLoader
-import androidx.loader.content.Loader
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.alelk.pws.database.provider.PwsDataProviderContract
-import com.alelk.pws.database.provider.PwsDataProviderContract.History.getContentUri
+import com.alelk.pws.database.DatabaseProvider
+import com.alelk.pws.database.dao.HistoryDao
+import com.alelk.pws.database.dao.HistoryItem
 import com.alelk.pws.pwapp.R
 import com.alelk.pws.pwapp.activity.PsalmActivity
 import com.alelk.pws.pwapp.adapter.HistoryRecyclerViewAdapter
-import com.alelk.pws.pwapp.fragment.HistoryFragment
+import kotlinx.coroutines.launch
 
 /**
  * History Fragment
  *
  * Created by Alex Elkin on 18.02.2016.
  */
-class HistoryFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
-  private var mRecyclerView: RecyclerView? = null
-  private var mHistoryAdapter: HistoryRecyclerViewAdapter? = null
-  private var mItemsLimit = 0
-  private var menuClearHistory: MenuItem? = null
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setHasOptionsMenu(true)
-  }
+class HistoryFragment : Fragment() {
+
+  private lateinit var recyclerView: RecyclerView
+  private lateinit var historyAdapter: HistoryRecyclerViewAdapter
+  private val historyViewModel: HistoryViewModel by viewModels()
 
   override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
+    inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    val v = inflater.inflate(R.layout.fragment_history, null)
-    mRecyclerView = v.findViewById(R.id.rv_history)
-    val layoutManager = LinearLayoutManager(activity)
-    mRecyclerView?.layoutManager = layoutManager
-    mHistoryAdapter =
-      HistoryRecyclerViewAdapter { psalmNumberId: Long ->
-        val intentPsalmView = Intent(activity, PsalmActivity::class.java)
-        intentPsalmView.putExtra(PsalmActivity.KEY_PSALM_NUMBER_ID, psalmNumberId)
-        startActivity(intentPsalmView)
-      }
-    mRecyclerView?.adapter = mHistoryAdapter
-    loaderManager.initLoader(PWS_HISTORY_LOADER, null, this)
-    return v
+    val view = inflater.inflate(R.layout.fragment_history, container, false)
+
+    recyclerView = view.findViewById(R.id.rv_history)
+    recyclerView.layoutManager = LinearLayoutManager(activity)
+
+    historyAdapter = HistoryRecyclerViewAdapter { psalmNumberId: Long ->
+      val intentPsalmView = Intent(activity, PsalmActivity::class.java)
+      intentPsalmView.putExtra(PsalmActivity.KEY_PSALM_NUMBER_ID, psalmNumberId)
+      startActivity(intentPsalmView)
+    }
+
+    recyclerView.adapter = historyAdapter
+
+    // Observe the history items from ViewModel
+    historyViewModel.historyItems.observe(viewLifecycleOwner) { historyItems ->
+      historyAdapter.submitList(historyItems)
+    }
+
+    return view
   }
 
-  @Deprecated("Deprecated in Java")
   override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-    menuClearHistory = menu.add(R.string.menu_clear_history)
-    menuClearHistory?.setIcon(R.drawable.ic_delete_black_24dp)
+    val menuClearHistory = menu.add(R.string.menu_clear_history)
+    menuClearHistory.setIcon(R.drawable.ic_delete_black_24dp)
+    menuClearHistory.setOnMenuItemClickListener {
+      historyViewModel.clearHistory()
+      true
+    }
     super.onCreateOptionsMenu(menu, inflater)
   }
 
-  @Deprecated("Deprecated in Java")
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    if (item.itemId == menuClearHistory!!.itemId) {
-      val deleted =
-        requireActivity().contentResolver.delete(PwsDataProviderContract.History.CONTENT_URI, null, null)
-      Log.d(LOG_TAG, "onOptionsItemSelected: remove history: removed $deleted items.")
-      return true
-    }
-    return super.onOptionsItemSelected(item)
-  }
-
-  override fun onAttach(context: Context) {
-    super.onAttach(context)
-    mItemsLimit = DEFAULT_ITEMS_LIMIT
-    if (arguments != null) {
-      mItemsLimit = requireArguments().getInt(KEY_ITEMS_LIMIT, DEFAULT_ITEMS_LIMIT)
-    }
-  }
-
-  override fun onCreateLoader(loaderId: Int, args: Bundle?): Loader<Cursor> =
-    when (loaderId) {
-      PWS_HISTORY_LOADER -> CursorLoader(
-        requireActivity(),
-        getContentUri(mItemsLimit),
-        null,
-        null,
-        null,
-        null
-      )
-      else -> throw java.lang.IllegalStateException("unable to create loader")
-    }
-
-  override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor) {
-    mHistoryAdapter!!.swapCursor(data)
-  }
-
-  override fun onLoaderReset(loader: Loader<Cursor>) {
-    mHistoryAdapter!!.swapCursor(null)
-  }
-
   companion object {
-    private val LOG_TAG = HistoryFragment::class.java.simpleName
-    const val KEY_ITEMS_LIMIT = "com.alelk.pws.pwapp.historyItemsLimit"
-    const val PWS_HISTORY_LOADER = 2
-    private const val DEFAULT_ITEMS_LIMIT = 100
-    fun newInstance(itemsLimit: Int): HistoryFragment {
-      val args = Bundle()
-      args.putInt(KEY_ITEMS_LIMIT, itemsLimit)
-      val fragment = HistoryFragment()
-      fragment.arguments = args
-      return fragment
+    fun newInstance(): HistoryFragment {
+      return HistoryFragment()
     }
+  }
+}
+
+class HistoryViewModel(application: Application) : AndroidViewModel(application) {
+
+  private val historyDao: HistoryDao = DatabaseProvider.getDatabase(application).historyDao()
+
+  val historyItems: LiveData<List<HistoryItem>> = historyDao.getAll().asLiveData()
+
+  fun clearHistory() = viewModelScope.launch {
+    historyDao.deleteAll()
   }
 }
