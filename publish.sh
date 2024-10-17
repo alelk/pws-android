29ddea2b93f71c95c17dba09f052fc6eb99dd177
Alex Elkin <alelkdev@gmail.com>
2024-10-17 23:21:04 +0300

    fix(publish): configure release publishing

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ export NEXT_RELEASE_VERSION
 if [[ $NEXT_RELEASE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo "Publishing $NEXT_RELEASE_VERSION as release"
   echo "$NEXT_RELEASE_VERSION" > app.version
+  ./gradlew :database:publishGprPublicationToGitHubPackages
 elif [[ $NEXT_RELEASE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-.+$ ]]; then
   echo "Publishing $NEXT_RELEASE_VERSION as snapshot"
   echo "${NEXT_RELEASE_VERSION%-*}-SNAPSHOT" > app.version
