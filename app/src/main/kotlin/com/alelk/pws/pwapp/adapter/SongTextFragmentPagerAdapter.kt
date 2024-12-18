22b144e1041fa38e5646336ab4e1f5f8fae03054
Mykhailo Dmytriakha <mykhailo_dmytriakha@bcbsil.com>
2024-12-17 22:57:54 -0800

    fix issue: copying wrong song from context menu

--- app/src/main/kotlin/com/alelk/pws/pwapp/adapter/SongTextFragmentPagerAdapter.kt
+++ app/src/main/kotlin/com/alelk/pws/pwapp/adapter/SongTextFragmentPagerAdapter.kt
@@ -27,13 +27,16 @@ import com.alelk.pws.pwapp.fragment.SongTextFragment
  */
 class SongTextFragmentPagerAdapter(
   fragmentActivity: FragmentActivity,
-  private var allSongNumberIds: List<Long>
+  allSongNumberIds: List<Long>
 ) : FragmentStateAdapter(fragmentActivity) {
 
+  private var _allSongNumberIds: List<Long> = allSongNumberIds
+  val allSongNumberIds: List<Long> get() = _allSongNumberIds
+
   fun setSongNumberIds(data: List<Long>) {
-    allSongNumberIds = data
+    _allSongNumberIds = data
   }
 
-  override fun getItemCount(): Int = allSongNumberIds.size
-  override fun createFragment(position: Int): Fragment = SongTextFragment.newInstance(allSongNumberIds[position])
+  override fun getItemCount(): Int = _allSongNumberIds.size
+  override fun createFragment(position: Int): Fragment = SongTextFragment.newInstance(_allSongNumberIds[position])
 }
