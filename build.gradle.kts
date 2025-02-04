2e19923e27198cbd762367136c7fcf06a1c851d0
Alex Elkin <alelkdev@gmail.com>
2025-02-05 00:19:19 +0300

    fix: update library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ allprojects {
   buildscript {
 
     dependencies {
-      classpath("com.android.tools.build:gradle:8.7.3")
+      classpath("com.android.tools.build:gradle:8.8.0")
       classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
   }
