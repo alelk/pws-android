af7af191c444f82c1f4363379291f86174a3c8b8
Alex Elkin <alelkdev@gmail.com>
2025-02-16 19:48:16 +0300

    test(database): fix failed database test

--- database/src/commonTest/kotlin/io/github/alelk/pws/database/entity/HistoryEntityArb.kt
+++ database/src/commonTest/kotlin/io/github/alelk/pws/database/entity/HistoryEntityArb.kt
@@ -8,17 +8,23 @@ import io.github.alelk.pws.domain.model.songId
 import io.github.alelk.pws.domain.model.songNumberId
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.constant
 import io.kotest.property.arbitrary.long
+import io.kotest.property.arbitrary.map
 import io.kotest.property.arbitrary.removeEdgecases
 import io.kotest.property.kotlinx.datetime.datetime
+import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 fun Arb.Companion.historyEntity(
   id: Arb<Long?>? = Arb.long(min = 0, max = 1_000_000_000).removeEdgecases(),
   bookId: Arb<BookId> = Arb.bookId(),
   songId: Arb<SongId> = Arb.songId(),
   songNumberId: Arb<SongNumberId> = Arb.songNumberId(bookId, songId),
-  accessTimestamp: Arb<LocalDateTime> = Arb.datetime(yearRange = 2025..2050)
+  // todo: library compatibility issue: accessTimestamp: Arb<LocalDateTime> = Arb.datetime(yearRange = 2025..2050),
+  accessTimestamp: Arb<LocalDateTime> = Arb.constant(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()))
 ): Arb<HistoryEntity> = arbitrary {
   HistoryEntity(
     id = id?.bind() ?: 0,
