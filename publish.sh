f82807b9a26c86d8effc184c90347a52b496ac87
Alex Elkin <alelkdev@gmail.com>
2025-02-16 22:41:11 +0300

    fix(ci): fix publish script

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ if [[ $NEXT_RELEASE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo "Publishing $NEXT_RELEASE_VERSION as release"
   echo "$NEXT_RELEASE_VERSION" > app.version
   ./gradlew :database:publishGprPublicationToGitHubPackages :domain:publishGprPublicationToGitHubPackages
-if [[ $NEXT_RELEASE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc.+$ ]]; then
+elif [[ $NEXT_RELEASE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc.+$ ]]; then
   echo "Publishing $NEXT_RELEASE_VERSION as pre-release"
   echo "$NEXT_RELEASE_VERSION" > app.version
   ./gradlew :database:publishGprPublicationToGitHubPackages :domain:publishGprPublicationToGitHubPackages
