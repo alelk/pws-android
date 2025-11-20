fe362f4389749d6a5cdebf9ad809598662355ad7
Alex Elkin <alelkdev@gmail.com>
2025-11-20 21:36:24 +0300

    fix(domain): fix CreateSongCommand

--- domain/src/commonMain/kotlin/io/github/alelk/pws/domain/song/command/CreateSongCommand.kt
+++ domain/src/commonMain/kotlin/io/github/alelk/pws/domain/song/command/CreateSongCommand.kt
@@ -5,9 +5,7 @@ import io.github.alelk.pws.domain.core.Locale
 import io.github.alelk.pws.domain.core.NonEmptyString
 import io.github.alelk.pws.domain.core.Version
 import io.github.alelk.pws.domain.core.Year
-import io.github.alelk.pws.domain.core.ids.BookId
 import io.github.alelk.pws.domain.core.ids.SongId
-import io.github.alelk.pws.domain.core.ids.TagId
 import io.github.alelk.pws.domain.person.Person
 import io.github.alelk.pws.domain.song.lyric.Lyric
 import io.github.alelk.pws.domain.tonality.Tonality
@@ -15,7 +13,6 @@ import io.github.alelk.pws.domain.tonality.Tonality
 /** Command to create a Song. **/
 data class CreateSongCommand(
   val id: SongId,
-  val bookId: BookId,
   val locale: Locale,
   val name: NonEmptyString,
   val lyric: Lyric,
@@ -24,7 +21,6 @@ data class CreateSongCommand(
   val translator: Person? = null,
   val composer: Person? = null,
   val tonalities: List<Tonality>? = null,
-  val tags: List<TagId> = emptyList(),
   val year: Year? = null,
   val bibleRef: BibleRef? = null,
   val edited: Boolean = false,
