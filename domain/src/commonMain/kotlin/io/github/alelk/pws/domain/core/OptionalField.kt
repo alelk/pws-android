3652819a316557b942de5067728bb804925c49f3
Alex Elkin <alelkdev@gmail.com>
2025-11-27 13:46:04 +0300

    feat(domain): Improve optional field api: add getOrElse helper method

--- domain/src/commonMain/kotlin/io/github/alelk/pws/domain/core/OptionalField.kt
+++ domain/src/commonMain/kotlin/io/github/alelk/pws/domain/core/OptionalField.kt
@@ -1,5 +1,8 @@
 package io.github.alelk.pws.domain.core
 
+import io.github.alelk.pws.domain.core.OptionalField.Clear
+import io.github.alelk.pws.domain.core.OptionalField.Unchanged
+
 /** Optional mutation semantic: Unchanged / Set(value) / Clear. */
 sealed interface OptionalField<out T> {
   data object Unchanged : OptionalField<Nothing>
@@ -13,9 +16,16 @@ sealed interface OptionalField<out T> {
       Clear -> block(null)
     }
 
+
   companion object {
     fun <T> fromNullable(nullable: T?, treatNullAsClear: Boolean = false): OptionalField<T> =
       if (nullable == null) if (treatNullAsClear) Clear else Unchanged else Set(nullable)
   }
+}
+
+inline fun <R, T : R> OptionalField<T>.getOrElse(defaultValue: () -> R): R = when (this) {
+  Unchanged -> defaultValue()
+  is OptionalField.Set -> value
+  Clear -> defaultValue()
 }
 
