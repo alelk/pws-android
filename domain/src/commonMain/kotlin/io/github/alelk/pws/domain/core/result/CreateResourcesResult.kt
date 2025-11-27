680b786f32c21b209cad531801371e9520bf5756
Alex Elkin <alelkdev@gmail.com>
2025-11-28 02:54:59 +0300

    feat(domain): Reimplement CreateResourcesResult domain model.

--- domain/src/commonMain/kotlin/io/github/alelk/pws/domain/core/result/CreateResourcesResult.kt
+++ domain/src/commonMain/kotlin/io/github/alelk/pws/domain/core/result/CreateResourcesResult.kt
@@ -3,7 +3,11 @@ package io.github.alelk.pws.domain.core.result
 sealed interface CreateResourcesResult<out R : Any> {
   data class Success<out R : Any>(val resources: List<R>) : CreateResourcesResult<R>
 
-  data class AlreadyExists<out R : Any>(val resource: R) : CreateResourcesResult<R>
+  /** Some resources from the input create command already exist.
+   *
+   * @property resources The resources from the input that were found to already exist
+   **/
+  data class AlreadyExists<out R : Any>(val resources: List<R>) : CreateResourcesResult<R>
 
   data class ValidationError<out R : Any>(val resource: R, val message: String) : CreateResourcesResult<R>
 
